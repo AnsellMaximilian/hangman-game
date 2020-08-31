8b62995146f86dda69a0a94f26c59d5fab9e2454
Auwnsullem <auwnsullem@gmail.com>
2020-08-31 18:48:14 +0700

    Updates

---
+++ precache-manifest.faf583acf7692599fd880615c7a2cfde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b545cf6d637f7eff94f6cb2d70d2db74",
+    "url": "/hangman-game/index.html"
+  },
+  {
+    "revision": "7a37fb12c20fcf952452",
+    "url": "/hangman-game/static/css/main.16c827fa.chunk.css"
+  },
+  {
+    "revision": "4915b4fb2488d92a8938",
+    "url": "/hangman-game/static/js/2.4eb5b616.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-game/static/js/2.4eb5b616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a37fb12c20fcf952452",
+    "url": "/hangman-game/static/js/main.6d0ac7fc.chunk.js"
+  },
+  {
+    "revision": "05a0ba7461ccdd6f1172",
+    "url": "/hangman-game/static/js/runtime-main.6dd32194.js"
+  },
+  {
+    "revision": "f5fdb27a1d66378cd9b805e4ac2097bb",
+    "url": "/hangman-game/static/media/chalkboard.f5fdb27a.jpg"
+  }
+]);
